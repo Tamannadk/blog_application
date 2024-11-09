13c0a5dda6be0fe52cb4bb7e92aa93e1030389a8
Tamannadk <tamannadk4@gmail.com>
2024-11-09 14:00:51 +0530

    create signup page ui

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 export const Signup = async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
@@ -7,15 +8,28 @@ export const Signup = async (req, res) => {
   if ([username, email, password].some((field) => field.trim() === "")) {
     return res.status(400).json({ message: "All fields are required!" });
   }
-  const newUser = await User.create({
-    username: username,
-    email: email,
-    password: password,
-  });
-  if (!newUser) {
-    return res
-      .status(400)
-      .json({ message: "Error while registering the user!" });
+
+  try {
+    //find user in the database first if the user is already logged in then no need to create again
+    const existingUser = await User.findOne({
+      $or: [{ username: username }, { email: email }],
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exist!" });
+    }
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    });
+    if (!newUser) {
+      return res
+        .status(400)
+        .json({ message: "Error while registering the user!" });
+    }
+    return res.status(200).json({ message: "User registered successfully!" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error!" });
   }
-  return res.status(200).json({ message: "User registered successfully!" });
 };
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import bcryptjs from "bcryptjs"
 const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
       required: true,
       unique: true,
+      lowercase:true
     },
     email: {
       type: String,
@@ -20,6 +22,11 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+
+userSchema.methods.isPasswordCorrect=async function (password) {
+  return  bcryptjs.compare(password,this.password)
+}
 const User = mongoose.model("User", userSchema);
 
  export default User;
